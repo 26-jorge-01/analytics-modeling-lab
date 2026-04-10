7953b80f98015ec652ae67aedc4f44f4f03a6bfb
26-jorge-01 <jonan0804@gmail.com>
2026-04-10 10:51:24 -0500

    chore: ignore dbt target folder and clean up untracked artifacts

--- transform/dbt/target/compiled/modeling_lab/models/marts/star/dim_customer.sql
+++
@@ -1,9 +0,0 @@
-with customers as (
-    select * from "modeling_lab"."public"."core_customers"
-)
-
-select
-    customer_id,
-    customer_unique_id,
-    customer_zip_code_prefix as zip_code_prefix
-from customers
